2126a024c3dc9f0b7598a210e87aa666d85f59f9
coffescript <dmejia1204@gmail.com>
2019-11-12 13:11:55 -0600

    create of components login and register for his respectives views

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    public page_title: string;
 
-  constructor() { }
+  constructor() { 
+    this.page_title = 'Login'
+  }
 
   ngOnInit() {
+
   }
 
 }
